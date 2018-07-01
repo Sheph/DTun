1f7b35cc901b70361f61a6efc97d81475a5fbc64
Stanislav Vorobiov <sheffmail@mail.ru>
2018-07-01 15:58:23 +0300

    dnode: initial commit of tun2socks stuff

--- dnode/main.cpp
+++ dnode/main.cpp
@@ -4,6 +4,8 @@
 #include <log4cplus/configurator.h>
 #include <iostream>
 
+extern "C" int tun2socks_main(int argc, char **argv);
+
 static void signalHandler(int sig)
 {
     LOG4CPLUS_INFO(DNode::logger(), "Signal " << sig << " received");
@@ -26,7 +28,10 @@ int main(int argc, char* argv[])
     DTun::SignalHandler sigHandler(&signalHandler);
 
     LOG4CPLUS_INFO(DNode::logger(), "Started");
+
+    int res = tun2socks_main(argc, argv);
+
     LOG4CPLUS_INFO(DNode::logger(), "Done");
 
-    return 0;
+    return res;
 }
