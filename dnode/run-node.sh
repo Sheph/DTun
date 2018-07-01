ae40a7754b7322e4def30cfe7594f99498e827b8
Stanislav Vorobiov <sheffmail@mail.ru>
2018-07-01 17:59:03 +0300

    dnode: drop sudo

--- dnode/run-node.sh
+++ dnode/run-node.sh
@@ -1,1 +1,1 @@
-sudo ./dnode --logger stdout --loglevel info --tundev tun0 --netif-ipaddr 10.0.0.2 --netif-netmask 255.255.255.0 --socks-server-addr localhost:4444
+./dnode --logger stdout --loglevel info --tundev tun0 --netif-ipaddr 10.0.0.2 --netif-netmask 255.255.255.0 --socks-server-addr localhost:4444
--- dnode/setup-node.sh
+++ dnode/setup-node.sh
@@ -1,4 +1,4 @@
-sudo ip tuntap add dev tun0 mode tun user root
+sudo ip tuntap add dev tun0 mode tun user stas
 sudo ifconfig tun0 10.0.0.1 netmask 255.255.255.0
 sudo route del default gw csp1.zte.com.cn
 sudo route add default gw csp1.zte.com.cn metric 6
