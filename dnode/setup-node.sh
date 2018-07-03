1744823d9b4b9a2f74aa6762a28871531a91ca21
stas <sheffmail@mail.ru>
2018-07-03 16:30:34 +0300

    dnode: loopback interface

--- dnode/setup-node.sh
+++ dnode/setup-node.sh
@@ -4,3 +4,4 @@ sudo ip tuntap add dev tun0 mode tun user stas
 sudo ip link set tun0 netns dnode1
 sudo ip netns exec dnode1 ifconfig tun0 10.0.0.1 netmask 255.255.255.0
 sudo ip netns exec dnode1 route add default gw 10.0.0.2
+sudo ip netns exec dnode1 ip link set dev lo up
